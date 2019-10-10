70c5d6b813335264665130724c64c19810d8b68e
Pablo <ambrogio408@gmail.com>
2019-10-09 21:12:35 -0300

    CAMBIO PABLO

--- Editorial-Proyecto/PROYECTO PRACTICAS BIBLIOTECA/Editorial-Proyecto/Editorial-Proyecto/FrmClientes.vb
+++ Editorial-Proyecto/PROYECTO PRACTICAS BIBLIOTECA/Editorial-Proyecto/Editorial-Proyecto/FrmClientes.vb
@@ -1,6 +1,9 @@
 ﻿Public Class FrmClientes
 
 
-    'dfgfdgsdfg
+    'PUTO EL QUE LEE
 
+    Private Sub FrmClientes_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
